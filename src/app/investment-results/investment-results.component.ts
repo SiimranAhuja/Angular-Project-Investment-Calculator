d74cd4939a34f3bcce39de18c52bef31eb288e63
Simran Ahuja <63255269+SiimranAhuja@users.noreply.github.com>
2024-11-06 12:35:29 +0530

    Add files via upload

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,34 @@
+import { Component, computed, inject } from '@angular/core';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+
+  // @Input() results?: {
+  //   year: number,
+  //   interest: number,
+  //   valueEndOfYear: number,
+  //   annualInvestment: number,
+  //   totalInterest: number,
+  //   totalAmountInvested: number,
+  // }[];
+
+  // results = input<{ year: number,
+  //     interest: number,
+  //     valueEndOfYear: number,
+  //     annualInvestment: number,
+  //     totalInterest: number,
+  //     totalAmountInvested: number}[]>()
+
+  private investmentService = inject(InvestmentService);
+
+  // get results() {
+  //   return this.investmentService.resultsData;
+  // }
+
+  results = computed(()=> this.investmentService.resultsData());
+}
